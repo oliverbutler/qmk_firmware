f52331695fb0df5b33581459a7cebff353f68152
Oliver Butler <dev@oliverbutler.uk>
2025-01-15 19:47:52 +0000

    feat: layout before change

--- keyboards/sofle/keymaps/oliverbutler/keymap.c
+++ keyboards/sofle/keymaps/oliverbutler/keymap.c
@@ -27,7 +27,7 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT(
   KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,
-  KC_ESC,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_RBRC,
+   KC_ESC,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_RBRC,
   KC_LSFT,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    LCTL_T(KC_SCLN),  KC_QUOT,
   KC_LCTL,  LT(_ARROW, KC_Z),   KC_X,    KC_C,    KC_V,    KC_B,  KC_MUTE,  XXXXXXX, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
         KC_LGUI, KC_LALT, KC_LCMD, KC_SPC, MO(_LOWER),  KC_ENT,  KC_BSPC, KC_RCTL, KC_RALT, KC_RGUI
@@ -40,15 +40,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                        _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______
 ),
 [_ARROW] = LAYOUT(
-  QK_BOOT, _______, _______, _______, _______, _______,                      _______, _______, _______, _______, _______, _______,
-// boot
-  CG_TOGG, _______, _______, _______, _______, _______,                      _______, _______, _______, _______, _______, _______,
-// os/swap
-  _______, _______, _______, _______, _______, _______,                      KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, _______, _______,
-//                                                                             left     down     up      right
-  _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______,
+      QK_BOOT, _______, _______, _______, _______, _______,                      _______, _______, _______, _______, _______, _______,
+//    boot
+      CG_TOGG, _______, _______, _______, _______, _______,                      _______, _______, _______, _______, _______, _______,
+//    os/swap
+      _______, _______, _______, _______, _______, _______,                      KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, _______, _______,
+//                                                                                left     down     up      right
+      _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______,
 //
-                    _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______
+                        _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______
 //
 
 ),
