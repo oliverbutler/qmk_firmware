20adc7eb411ea265ca62e8bd363ddf0473c0f55c
Oliver Butler <dev@oliverbutler.uk>
2025-03-24 09:15:13 +0000

    fix: try solve enter tapping issues

--- keyboards/sofle/keymaps/oliverbutler/config.h
+++ keyboards/sofle/keymaps/oliverbutler/config.h
@@ -4,7 +4,20 @@
 #pragma once
 
 // Configure TAPPING_TERM
-#define TAPPING_TERM 200
+// The time (in milliseconds) that a key must be held to register as a "hold" action
+// Increased from default 200ms to make taps more reliable, especially for the Enter key
+// Higher values make it easier to register taps, but might make holds feel slightly delayed
+#define TAPPING_TERM 225
+
+// When enabled, releasing a dual-function key without pressing another key will
+// always send the tap action (Enter in your case), even if you held it longer than TAPPING_TERM
+// This prevents "lost" Enter keypresses when you hold slightly too long
+#define RETRO_TAPPING
+
+// If you tap a dual-function key twice quickly (within this many milliseconds),
+// the second press will be registered as a tap rather than starting a hold
+// This makes double-tapping Enter work reliably even if you're typing quickly
+#define QUICK_TAP_TERM 120
 
 #define TRI_LAYER_LOWER_LAYER 2
 #define TRI_LAYER_UPPER_LAYER 3
